5aede049f860a0abf47270a78605e4a19ece5842
Adeyemi Olaoye <yhemi06@gmail.com>
2024-08-30 07:57:50 +0100

    completed the connectwallet function

--- services/blockChain.ts
+++ services/blockChain.ts
@@ -3,9 +3,9 @@ import { ethers } from 'ethers'
 import { globalActions } from '@/store/globalSlices'
 import address from '@/artifacts/contractAddress.json'
 import abi from '@/artifacts/contracts/AnswerToEarn.sol/AnswerToEarn.json'
-import { AnswerProp, QuestionParams, QuestionProp } from '@/utils/interfaces'
+import { AnswerProp, QuestionProp } from '@/utils/interfaces'
 
-const { setWallet, setAnswers, setQuestion, setQuestions } = globalActions
+const { setWallet } = globalActions
 const ContractAddress = address.address
 const ContractAbi = abi.abi
 let ethereum: any
@@ -21,6 +21,8 @@ const fromWei = (num: number) => ethers.utils.formatEther(num)
 const connectWallet = async () => {
   try {
     if (!ethereum) return reportError('Please install Metamask')
+    const accounts = await ethereum.request?.({ method: 'eth_requestAccounnts' })
+    store.dispatch(setWallet(accounts?.[0]))
   } catch (error) {}
 }
 
